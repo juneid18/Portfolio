550232e0326334bbdc83767b540e42f359a48f2e
Juneid Shaikh <90983910+juneid18@users.noreply.github.com>
2024-10-03 00:39:21 +0530

    Update aboutMe.js

--- app/components/AboutMe/aboutMe.js
+++ app/components/AboutMe/aboutMe.js
@@ -1,6 +1,6 @@
 "use client";
 import styles from "./aboutMe.module.css";
-import { FaLinkedin , FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
 
 const AboutMe = () => {
   return (
@@ -24,24 +24,57 @@ const AboutMe = () => {
           <br />
           <b>Address: </b>
           <span>Satara, Maharashtra, India</span>
-          <br /><br />
-          <a className={styles.resumebtn} href="https://drive.google.com/file/d/1hqvmQRI_ecYpaJEnpZyhYnnLt-LUKfbt/view?usp=sharing" target="_blank">Download My Resume</a>
+          <br />
+          <br />
+          <a
+            className={styles.resumebtn}
+            href="https://docs.google.com/document/d/1q_iQxEJ_6QvCIpPuizzdaMZublnJhaOOIP2YBBuW7I0/edit?usp=sharing"
+            target="_blank"
+          >
+            Download My Resume
+          </a>
         </div>
         <div className={styles.about_container_right}>
           <h2>Hello There!</h2>
           <p>
-            Experienced Full Stack Developer proficient in React.js, Next.js, MongoDB, and more. With a proven track record in crafting user-centric web applications, I specialize in delivering high-quality, scalable solutions that exceed expectations. From personalized e-commerce platforms to real-time chat applications, I bring creativity and technical expertise to every project.
+            Experienced Full Stack Developer proficient in React.js, Next.js,
+            MongoDB, and more. With a proven track record in crafting
+            user-centric web applications, I specialize in delivering
+            high-quality, scalable solutions that exceed expectations. From
+            personalized e-commerce platforms to real-time chat applications, I
+            bring creativity and technical expertise to every project.
           </p>
 
           <br />
           <p>
-            My dedication to optimizing performance and user experience has resulted in tangible outcomes, including increased engagement and enhanced customer satisfaction. With a keen eye for detail and a passion for innovation, I am committed to driving success in collaborative environments. Seeking opportunities to leverage my skills and contribute to impactful projects. Let&apos;s connect and explore how I can add value to your team.
+            My dedication to optimizing performance and user experience has
+            resulted in tangible outcomes, including increased engagement and
+            enhanced customer satisfaction. With a keen eye for detail and a
+            passion for innovation, I am committed to driving success in
+            collaborative environments. Seeking opportunities to leverage my
+            skills and contribute to impactful projects. Let&apos;s connect and
+            explore how I can add value to your team.
           </p>
-          <a href="https://www.linkedin.com/in/juneid-shaikh/" className={styles.socialbtn}>
+          <a
+            href="https://www.linkedin.com/in/juneid-shaikh/"
+            className={styles.socialbtn}
+            target="_blank"
+          >
             <FaLinkedin />
           </a>
-          <a href="https://x.com/Juneidshaikh18?t=4w1-K7AXflxO848dM-5pqg&s=09" className={styles.socialbtn}>
+          <a
+            href="https://x.com/Juneidshaikh18?t=4w1-K7AXflxO848dM-5pqg&s=09"
+            className={styles.socialbtn}
+            target="_blank"
+          >
             <FaTwitter />
+          </a>
+          <a
+            href="https://github.com/juneid18"
+            className={styles.socialbtn}
+            target="_blank"
+          >
+            <FaGithub />
           </a>
         </div>
       </div>
